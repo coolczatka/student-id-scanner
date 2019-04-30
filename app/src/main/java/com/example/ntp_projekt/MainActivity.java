9efa2870a210ad04ce34b2cc8ee8bef16318b2cc
coolczatka <karolbaran0@gmail.com>
2019-04-30 10:32:28 +0200

    reading card id

--- app/src/main/java/com/example/ntp_projekt/MainActivity.java
+++ app/src/main/java/com/example/ntp_projekt/MainActivity.java
@@ -1,30 +1,83 @@
 package com.example.ntp_projekt;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.nfc.NfcAdapter;
 import android.app.PendingIntent;
+import android.content.Intent;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-   protected NfcAdapter adapter;
-   TextView tv;
-
-   protected void cnstr(){
-       this.adapter = NfcAdapter.getDefaultAdapter(this);
-       this.tv = findViewById(R.id.textfield);
-       if(!this.adapter.isEnabled())
-           this.tv.setText("Włącz nfc");
-       else
-           this.tv.setText("jest wlączone pozdro");
-       this.adapter.
-   }
+    private NfcAdapter nfcAdapter;
+    PendingIntent mPendingIntent;
+    TextView textViewInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        cnstr();
+        textViewInfo = findViewById(R.id.info);
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        mPendingIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        if(nfcAdapter == null){
+            Toast.makeText(this,
+                    "NFC NOT supported on this devices!",
+                    Toast.LENGTH_LONG).show();
+            finish();
+        }else if(!nfcAdapter.isEnabled()){
+            Toast.makeText(this,
+                    "NFC NOT Enabled!",
+                    Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        nfcAdapter.enableForegroundDispatch(this, mPendingIntent, null, null);
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        nfcAdapter.disableForegroundDispatch(this);
+    }
+    @Override
+    public void onNewIntent(Intent intent){
+        setIntent(intent);
+        resolveIntent(intent);
+    }
+    private void resolveIntent(Intent intent){
+        String action = intent.getAction();
+
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)) {
+            Toast.makeText(this,
+                    "onResume() - ACTION_TAG_DISCOVERED",
+                    Toast.LENGTH_SHORT).show();
+            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            byte[] tagId = intent.getByteArrayExtra(NfcAdapter.EXTRA_ID);
+
+            if(tag == null){
+                textViewInfo.setText("tag == null");
+            }else{
+                String tagInfo = tag.toString() + "\n";
+
+                tagInfo += "\nTag Id: \n";
+                tagInfo += "dlugosc = " + tagId.length +" bajty\n";
+                for(int i=0; i<tagId.length; i++){
+                    tagInfo += Integer.toHexString(tagId[i] & 0xFF) + " ";
+                }
+
+                textViewInfo.setText(tagInfo);
+            }
+        }else{
+            Toast.makeText(this,
+                    "onResume() : " + action,
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 }
